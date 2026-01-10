d18def51533863fd15337c49ac298f3033346386
MASNADA GIORGIO <MASNADA.21197>
2026-01-10 09:16:33 +0100

    mappe

--- js/point-06.js
+++ js/point-06.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // Coordinate dell'Istituto Tecnico Vittorio Emanuele II
-    const coordinates = [45.69255919897298, 9.67478728735614];
+    const coordinates = [45.69675345969597, 9.67369090785965];
 
     // Inizializza la mappa
     const map = L.map('map').setView(coordinates, 16);
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Marker
     L.marker(coordinates)
         .addTo(map)
-        .bindPopup("<b>Istituto Tecnico Vittorio Emanuele II</b><br>Bergamo")
+        .bindPopup("<b>Scuola media 'Eugenio Donadoni'</b><br>Bergamo")
         .openPopup();
 
     // Correzione rendering (utile se la mappa è in un container nascosto)
