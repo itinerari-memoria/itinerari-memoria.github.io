0a1dd4d0762b8bc74a6e92a4ded6274f93d49441
Taiocchi <luc.taio07@gmail.com>
2026-04-09 12:06:40 +0200

    Tolto script lingua

--- js/lingua.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const userLang = navigator.language || navigator.userLanguage;
-    const currentPage = window.location.pathname;
-
-    // Se lingua NON italiana e pagina attuale NON è index-en.html
-    if (!userLang.toLowerCase().startsWith('it') && !currentPage.endsWith('index-en.html')) {
-        window.location.href = currentPage.replace('index.html', 'index-en.html');
-    }
-})();
